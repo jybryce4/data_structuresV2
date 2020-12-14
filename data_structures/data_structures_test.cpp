99b8fc7b324eb2ede8208f8089b73601b56a140a
James <jybryce4@gmail.com>
2020-12-14 13:24:48 -0600

    made list_tester function in test.h

--- data_structures/data_structures_test.cpp
+++ data_structures/data_structures_test.cpp
@@ -1,14 +1,7 @@
 #include "test.h"
 
 int main() {
-	data_structures::list<int> my_list;
-
-	for (int i = 0; i < 10; i++) {
-	    my_list.add_node(i);
-	}
-	
-	my_list.fwd_traverse();
-
+    list_tester();
 
 	return 0;
 }
--- data_structures/test.h
+++ data_structures/test.h
@@ -8,4 +8,28 @@
 using std::cout;
 using std::endl;
 
+void list_tester() {
+    data_structures::list<int> my_list;
+    data_structures::list<int> my_other_list;
+
+    for (int i = 0; i < 10; i++) {
+        my_list.add_node(i);
+    }
+
+    for (int j = 10; j < 20; j++) {
+        my_other_list.add_node(j);
+    }
+
+    cout << "my_list.fwd_traverse(): ";
+    my_list.fwd_traverse();
+
+    cout << "my_other_list.fwd_traverse(): ";
+    my_other_list.fwd_traverse();
+
+    my_list = my_other_list;
+
+    cout << "Copied my_other_list into my_list..." << endl;
+    my_list.fwd_traverse();
+}
+
 #endif
