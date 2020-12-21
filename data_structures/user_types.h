505a2c30f70077b94c7f79e9a6f3af11be2793c3
James <jybryce4@gmail.com>
2020-12-20 21:12:01 -0600

    fixed list copy constructor changed namespace for list tests from test to list_tests some readability improvements added a user_types file for types i'll be using to test my data structures with

---
+++ data_structures/user_types.h
@@ -0,0 +1,25 @@
+#ifndef DATA_STRUCTURES_USER_TYPES_H
+#define DATA_STRUCTURES_USER_TYPES_H
+
+#include <ostream>
+#include <string>
+
+namespace user_types {
+    class Book {
+    private:
+        long long m_isbn;
+        std::string m_title;
+        std::string m_author;
+
+    public:
+        Book(std::string title, std::string author, long long isbn);
+
+        long long get_isbn() const;
+        std::string get_title() const;
+        std::string get_author() const;
+
+        std::ostream &operator<<(const Book &book);
+    };
+} // namespace user_types
+
+#endif
